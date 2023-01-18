10f2765d37209c54d82c6ea697e2829d134433e9
jkracz <joseph.kracz@gmail.com>
2023-01-17 20:34:59 -0800

    Created the directory and category item components. Also added the styles for each

---
+++ src/components/category-item/category-item.component.jsx
@@ -0,0 +1,17 @@
+import "./category-item.styles.scss"
+
+const CategoryItem = ({category}) => {
+    return (
+        <div key={category.id} className="category-container">
+        <div className="background-image" style={{
+          backgroundImage: `url(${category.imageUrl})`
+        }} />
+        <div className="category-body-container">
+          <h2>{category.title}</h2>
+          <p>shop now</p>
+        </div>
+      </div>
+    )
+}
+
+export default CategoryItem
