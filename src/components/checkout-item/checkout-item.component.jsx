edf06615965b41bfa21973acd6100b8b9e5a8b2b
Joe Kracz <joseph.kracz@gmail.com>
2023-03-14 20:58:38 -0700

    styled the checkout items and added the functionality to clear items from the cart

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -5,15 +5,20 @@ import { CartContext } from "../../contexts/cart.context";
 
 const CheckoutItem = ({ cartItem }) => {
     const { name, imageUrl, price, quantity } = cartItem;
-    const { addItemToCart, removeItemFromCart } = useContext(CartContext);
+    const { addItemToCart, removeItemFromCart, clearItemFromCart } = useContext(CartContext);
     return (
-        <div>
-            <img src={imageUrl} alt={name} />
-            <span>{name}</span>
-            <span>{quantity}</span>
-            <span>{price}</span>
-            <button onClick={() => { addItemToCart(cartItem) }}>INCREMENT</button>
-            <button onClick={() => { removeItemFromCart(cartItem) }}>DECREMENT</button>
+        <div className="checkout-item-container">
+            <div className="image-container">
+                <img src={imageUrl} alt={name} />
+            </div>
+            <span className="name">{name}</span>
+            <span className="quantity">
+                <div className="arrow" onClick={() => { removeItemFromCart(cartItem) }}>&#10094;</div>
+                <span className="value">{quantity}</span>
+                <div className="arrow" onClick={() => { addItemToCart(cartItem) }}>&#10095;</div>
+            </span>
+            <span className="price">{price}</span>
+            <div className="remove-button" onClick={() => clearItemFromCart(cartItem)} >&#10005;</div>
         </div>
     )
 }
--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -30,13 +30,20 @@ const removeCartItem = (cartItems, cartItemToRemove) => {
     return updatedCart.filter(cartItem => cartItem.quantity > 0);
 }
 
+
+const clearCartItem = (cartItems, cartItemToClear) => {
+    return cartItems.filter((cartItem) => cartItem.id !== cartItemToClear.id);
+}
+
 export const CartContext = createContext({
     isCartOpen: false,
     setIsCartOpen: () => { },
     cartItems: [],
     addItemToCart: () => { },
     cartQuantity: 0,
-    setCartQuantity: () => { }
+    setCartQuantity: () => { },
+    clearItemFromCart: () => { },
+    total: 0
 });
 
 // the wrapper that provides the value
@@ -44,6 +51,7 @@ export const CartProvider = ({ children }) => {
     const [isCartOpen, setIsCartOpen] = useState(false);
     const [cartItems, setCartItems] = useState([]);
     const [cartQuantity, setCartQuantity] = useState(0);
+    const [cartTotal, setTotal] = useState(0);
 
     const addItemToCart = (productToAdd) => {
         setCartItems(addCartItem(cartItems, productToAdd));
@@ -53,12 +61,21 @@ export const CartProvider = ({ children }) => {
         setCartItems(removeCartItem(cartItems, cartItemToRemove));
     }
 
+    const clearItemFromCart = (cartItemToRemove) => {
+        setCartItems(clearCartItem(cartItems, cartItemToRemove));
+    }
+
     useEffect(() => {
         const itemsInCart = cartItems.reduce((accumulator, item) => accumulator + item.quantity, 0);
         setCartQuantity(itemsInCart);
     }, [cartItems]);
 
-    const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems, cartQuantity, removeItemFromCart };
+    useEffect(() => {
+        const itemTotal = cartItems.reduce((accumulator, item) => accumulator + (item.quantity * item.price), 0);
+        setTotal(itemTotal);
+    }, [cartItems]);
+
+    const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems, cartQuantity, removeItemFromCart, clearItemFromCart, cartTotal };
 
     // you pass the state value and the setter so any child component can use it
     return <CartContext.Provider value={value}>{children}</CartContext.Provider>
