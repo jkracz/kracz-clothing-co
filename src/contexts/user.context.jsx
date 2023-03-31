f54205c269f01d6de1cb898aa5e9040932d17c65
Joe Kracz <joseph.kracz@gmail.com>
2023-03-31 11:26:29 -0700

    used a reducer for the user context; first time playing with reducers

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,4 +1,4 @@
-import { useState, createContext, useEffect } from "react";
+import { createContext, useEffect, useReducer } from "react";
 import { onAuthStateChangedListener, createUserDocFromAuth } from "../utils/firebase/firebase.utils";
 
 // the actual value you want to access
@@ -7,9 +7,40 @@ export const UserContext = createContext({
     setCurrentUser: () => null,
 });
 
+export const USER_ACTION_TYPES = {
+    "SET_CURRENT_USER": "SET_CURRENT_USER"
+}
+
+const userReducer = (state, action) => {
+    const { type, payload } = action;
+
+    switch(type) {
+        case "SET_CURRENT_USER":
+            return {
+                ...state,
+                currentUser: payload
+            }
+        default:
+            throw new Error(`Unhandled type ${type} in userReducer`);
+    }
+}
+
+const INITIAL_STATE = {
+    currentUser: null
+}
+
 // the wrapper that provides the value
 export const UserProvider = ({ children }) => {
-    const [currentUser, setCurrentUser] = useState(null);
+    const [ state, dispatch ] = useReducer(userReducer, INITIAL_STATE) // hook takes the reducer you want and it's initial state
+    // a dispatch func is returned which is how you call the reducer
+
+    const { currentUser } = state; // setting the current user returned by reducer hook (this will be used by other parts of app)
+    const setCurrentUser = (user) => {
+        // when you use the dispatch function, you just want to pass it the action obj
+        // the actual reducer will maintain current state
+        dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
+    }
+
     const value = {currentUser, setCurrentUser};
 
     useEffect(() => {
