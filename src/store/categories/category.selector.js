0778f719af7885ee6b6746c7318807cc2e84f2b7
Joe Kracz <joseph.kracz@gmail.com>
2023-04-06 10:33:28 -0700

    added the reselect library to prevent the category component from rerendering when I sign out as a user

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,9 +1,31 @@
-export const selectCategoriesMap = (state) => {
-    const categoryMap = state.categories.categories.reduce((acc, category) => {
-        const { title, items } = category;
-        acc[title.toLowerCase()] = items;
-        return acc;
-    }, {});
+import { createSelector } from "reselect";
 
-    return categoryMap;
-};
+// a selector that returns the categories reducer
+const selectCategoryReducer = (state) => state.categories;
+
+
+// a selector that returns the categories array
+// the selector caches the category reducer & the prev output of the func
+// if the reducer stays the same, the cached prev value is returned; otherwise the func runs and recalcs the val
+export const selectCategories = createSelector(
+    [selectCategoryReducer],
+    (categoriesSlice) => categoriesSlice.categories
+);
+
+
+// a selector that returns the category map
+// the categories array from the output of selectCategories is cached
+// the prev return val of the func is also cached
+// if the array is different, then the func runs; otherwise the prev cached val is returned
+export const selectCategoriesMap = createSelector(
+    [selectCategories],
+    (categories) => {
+        console.log("eat my feet")
+        const categoryMap = categories.reduce((acc, category) => {
+            const { title, items } = category;
+            acc[title.toLowerCase()] = items;
+            return acc;
+        }, {});
+        return categoryMap;
+    }
+)
